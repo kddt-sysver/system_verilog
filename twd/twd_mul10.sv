0e471d1033ba36bb3e3a66fd76a27efb69c16990
songsong <71217543+SongYuGyeong@users.noreply.github.com>
2025-07-29 21:50:48 +0900

    Update twd_mul10.sv

--- twd/twd_mul10.sv
+++ twd/twd_mul10.sv
@@ -1,13 +1,14 @@
+
 `timescale 1ns / 1ps
 
 module twd_mul10 #(
     parameter int WIDTH = 12,
-    parameter int CLK_CNT = 4
+    CLK_CNT = 4
 ) (
     input logic clk,
     input logic rstn,
     input logic twd10_valid,
-    input logic signed [WIDTH-1:0] i_10bfly_sum_re[0:15],  //<6.6>
+    input logic signed [WIDTH-1:0] i_10bfly_sum_re[0:15],  // <6.6>
     input logic signed [WIDTH-1:0] i_10bfly_sum_im[0:15],
     input logic signed [WIDTH-1:0] i_10bfly_diff_re[0:15],
     input logic signed [WIDTH-1:0] i_10bfly_diff_im[0:15],
@@ -18,21 +19,21 @@ module twd_mul10 #(
 );
     int i, j;
     logic [3:0] twd_10_cnt;
-    logic [3:0] twd_10_idx;
+    logic [1:0] twd_10_idx;
 
     counter #(
         .COUNT_MAX_VAL(CLK_CNT)
     ) U_TWD_10_CNT (
         .clk      (clk),
         .rstn     (rstn),
-        .en       (twd10_valid),           
+        .en       (twd10_valid),          
         .count_out(twd_10_cnt)
     );
-    assign twd_10_idx = twd_10_cnt;
+    assign twd_10_idx = twd_10_cnt / 8;
 
     always @(*) begin
         case (twd_10_idx)
-            0, 1, 2: begin
+            0: begin
                 for (j = 0; j < 16; j++) begin
                     twd_10_sum_re[j]  = i_10bfly_sum_re[j];
                     twd_10_sum_im[j]  = i_10bfly_sum_im[j];
@@ -40,7 +41,7 @@ module twd_mul10 #(
                     twd_10_diff_im[j] = i_10bfly_diff_im[j];
                 end
             end
-            3: begin
+            1: begin
                 for (j = 0; j < 16; j++) begin
                     twd_10_sum_re[j]  = i_10bfly_sum_re[j];
                     twd_10_sum_im[j]  = i_10bfly_sum_im[j];
@@ -48,6 +49,7 @@ module twd_mul10 #(
                     twd_10_diff_im[j] = -i_10bfly_diff_re[j];
                 end
             end
+            
             default: begin
                 twd_10_sum_re[j]  = i_10bfly_sum_re[j];
                 twd_10_sum_im[j]  = i_10bfly_sum_im[j];
@@ -57,3 +59,4 @@ module twd_mul10 #(
         endcase
     end
 endmodule
+
