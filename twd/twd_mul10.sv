5c55ea670907378c118785cd9d15c3fa38e27709
songsong <71217543+SongYuGyeong@users.noreply.github.com>
2025-07-29 19:32:56 +0900

    Update twd_mul10.sv

--- twd/twd_mul10.sv
+++ twd/twd_mul10.sv
@@ -28,7 +28,7 @@ module twd_mul10 #(
         .en       (twd10_valid),           
         .count_out(twd_10_cnt)
     );
-    assign twd_10_idx = twd_10_cnt
+    assign twd_10_idx = twd_10_cnt;
 
     always @(*) begin
         case (twd_10_idx)
